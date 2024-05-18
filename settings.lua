bc631ee6ad924fe417fe94674c8f0a5ad836edbd
NathanSnail <47441022+NathanSnail@users.noreply.github.com>
2024-05-19 11:44:10 +1200

    disable mod restriction

--- settings.lua
+++ settings.lua
@@ -2,24 +2,30 @@ dofile("data/scripts/lib/mod_settings.lua")
 
 local mod_id = "noita_engine_patcher" -- This should match the name of your mod's folder.
 mod_settings_version = 1 -- This is a magic global that can be used to migrate settings to new mod versions. call mod_settings_get_version() before mod_settings_update() to get the old value.
-mod_settings = {
-	{
-		id = "frames",
-		ui_name = "Stats in frames",
+
+local function generate_setting(id, name)
+	return {
+		id = id,
+		ui_name = name,
 		value_default = false,
 		ui_fn = function(mod_id, gui, in_main_menu, im_id, setting)
 			GuiLayoutBeginHorizontal(gui, 0, 0)
-			GuiText(gui, 0, 0, "Stats in frames: ")
-			local state = ModSettingGet(mod_id .. ".frames") or false
+			GuiText(gui, 0, 0, name .. ": ")
+			local state = ModSettingGet(mod_id .. "." .. id) or false
 			if GuiButton(gui, im_id, 0, 0, tostring(state)) then
 				state = not state
-				ModSettingSetNextValue(mod_id .. ".frames", state, false)
-				ModSettingSet(mod_id .. ".frames", state)
+				ModSettingSetNextValue(mod_id .. "." .. id, state, false)
+				ModSettingSet(mod_id .. "." .. id, state)
 			end
 			GuiLayoutEnd(gui)
 		end,
 		scope = MOD_SETTING_SCOPE_RUNTIME,
-	},
+	}
+end
+
+mod_settings = {
+	generate_setting("frames", "Stats in frames"),
+	generate_setting("mods", "Disable mod restrictions"),
 }
 
 -- This function is called to ensure the correct setting values are visible to the game. your mod's settings don't work if you don't have a function like this defined in settings.lua.
