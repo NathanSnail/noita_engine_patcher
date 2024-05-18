2269c07fd763d6f1bda754597edc94e688c0fc3f
NathanSnail <47441022+NathanSnail@users.noreply.github.com>
2024-05-18 21:44:24 +1200

    uhhh

---
+++ settings.lua
@@ -0,0 +1,39 @@
+dofile("data/scripts/lib/mod_settings.lua")
+
+local mod_id = "noita_engine_patcher" -- This should match the name of your mod's folder.
+mod_settings_version = 1 -- This is a magic global that can be used to migrate settings to new mod versions. call mod_settings_get_version() before mod_settings_update() to get the old value.
+mod_settings = {
+	{
+		id = "frames",
+		ui_name = "Stats in frames",
+		value_default = false,
+		ui_fn = function(mod_id, gui, in_main_menu, im_id, setting)
+			GuiText(gui, 0, 0, "Stats in frames: ")
+			local state = ModSettingGet("noita_engine_patcher.frames")
+			if GuiButton(gui, im_id, 0, 0, tostring(state)) then
+				state = not state
+				ModSettingSetNextValue(mod_id .. "frames", state, false)
+				ModSettingSet(mod_id .. "frames", state)
+			end
+		end,
+		scope = MOD_SETTING_SCOPE_RUNTIME_RESTART,
+	},
+}
+
+-- This function is called to ensure the correct setting values are visible to the game. your mod's settings don't work if you don't have a function like this defined in settings.lua.
+function ModSettingsUpdate(init_scope)
+	local old_version = mod_settings_get_version(mod_id) -- This can be used to migrate some settings between mod versions.
+	mod_settings_update(mod_id, mod_settings, init_scope)
+end
+
+-- This function should return the number of visible setting UI elements.
+-- Your mod's settings wont be visible in the mod settings menu if this function isn't defined correctly.
+-- If your mod changes the displayed settings dynamically, you might need to implement custom logic for this function.
+function ModSettingsGuiCount()
+	return mod_settings_gui_count(mod_id, mod_settings)
+end
+
+-- This function is called to display the settings UI for this mod. your mod's settings wont be visible in the mod settings menu if this function isn't defined correctly.
+function ModSettingsGui(gui, in_main_menu)
+	mod_settings_gui(mod_id, mod_settings, gui, in_main_menu)
+end
